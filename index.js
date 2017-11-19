f57741bd95a00ed5207936f372bd91f70e8192c3
kylenzhong <kylenzhong@gmail.com>
2017-11-19 15:44:53 -0800

    fixed index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', function(req, res){
 
 
 app.get('/webhook/', function(req, res){
-	if(req.query['hub.verify_token'] === "blondiebytes"){
+	if(req.query['hub.verify_token'] === "sherlockbot"){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("wrong token")
